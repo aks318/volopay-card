b82cc0d97526a01294c196d357224012c8d9b43d
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-06-07 22:10:56 +0530

    card seperated

--- src/components/VolopayCard.js
+++ src/components/VolopayCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {useDispatch , useSelector} from 'react-redux'
 import { getCardData } from '../redux-store/action'
+import Card from './Card'
 import './VolopayCard.css'
 
 const VolopayCard = () => {
@@ -15,34 +16,8 @@ const VolopayCard = () => {
 
   return (
     <div className='container'>
-        {cardData.map((data) =>(
-            <div className='card'>
-                <div className='card-top'>
-                    <div className='card-owner'>
-                        <h4>{data.name}</h4>
-                        <p>{data.budget_name}</p>
-                    </div>
-                    <img src={data.card_type === 'burner' ? './images/burner.png' : './images/subscription.png'}/>
-                </div>
-                <div className='card-type'>
-                    <p>{data.card_type}</p>
-                    <p>Expires: {data.expiry}</p>
-                </div>
-                <div className='spent-bar'>
-                    <hr style={{width : `${(data.spent.value/(data.available_to_spend.value + data.spent.value))*100}%` , backgroundColor : "red"}}></hr>
-                    <hr style={{width : `${(data.available_to_spend.value/(data.available_to_spend.value + data.spent.value))*100}%` , backgroundColor : "green"}}></hr>
-                </div>
-                <div style={{marginTop : "10px"}}>
-                    <div className='balance'>
-                        <p><span className="dot" style={{backgroundColor : "red"}}></span>Spent</p>
-                        <p>{data.spent.value} SGD</p>
-                    </div>
-                    <div className='balance'>
-                        <p><span className="dot" style={{backgroundColor : "green"}}></span>Available to spend</p>
-                        <p>{data.available_to_spend.value} SGD</p>
-                    </div>
-                </div>
-            </div>
+        {cardData.map((data , index) =>(
+            <Card key={index} data={data}/>
         ))}
     </div>
   )
