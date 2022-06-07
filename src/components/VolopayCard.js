18a2d602ca86f37f421c4ebdab02bbaa99cf6268
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-06-07 21:27:16 +0530

    card done

--- src/components/VolopayCard.js
+++ src/components/VolopayCard.js
@@ -22,7 +22,7 @@ const VolopayCard = () => {
                         <h4>{data.name}</h4>
                         <p>{data.budget_name}</p>
                     </div>
-                    <img src='./images/burner.png'/>
+                    <img src={data.card_type === 'burner' ? './images/burner.png' : './images/subscription.png'}/>
                 </div>
                 <div className='card-type'>
                     <p>{data.card_type}</p>
@@ -31,6 +31,16 @@ const VolopayCard = () => {
                 <div className='spent-bar'>
                     <hr style={{width : `${(data.spent.value/(data.available_to_spend.value + data.spent.value))*100}%` , backgroundColor : "red"}}></hr>
                     <hr style={{width : `${(data.available_to_spend.value/(data.available_to_spend.value + data.spent.value))*100}%` , backgroundColor : "green"}}></hr>
+                </div>
+                <div style={{marginTop : "10px"}}>
+                    <div className='balance'>
+                        <p><span className="dot" style={{backgroundColor : "red"}}></span>Spent</p>
+                        <p>{data.spent.value} SGD</p>
+                    </div>
+                    <div className='balance'>
+                        <p><span className="dot" style={{backgroundColor : "green"}}></span>Available to spend</p>
+                        <p>{data.available_to_spend.value} SGD</p>
+                    </div>
                 </div>
             </div>
         ))}
