82ab73b4d5ab07157a3f4679a881edb493846d16
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-06-08 22:05:01 +0530

    FilterBox added

--- src/components/VolopayCard.js
+++ src/components/VolopayCard.js
@@ -3,13 +3,18 @@ import AllCard from './pages/AllCard'
 import { Input } from 'antd';
 import IconButton from '@mui/material/IconButton';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { Select } from 'antd';
 import './VolopayCard.css'
 import YourCard from './pages/YourCard';
 
+const { Option } = Select;
 const Search = Input.Search;
 const VolopayCard = () => {
     const [activeLink , setActiveLink] = useState("All")
     const [search , setSearch] = useState("")
+    const [openFilter , setOpenFilter] = useState(false)
 
   return (
       <div className='main-div'>
@@ -39,9 +44,32 @@ const VolopayCard = () => {
                 <IconButton
                     aria-label="expand row"
                     size="small"
+                    onClick={() => setOpenFilter(!openFilter)}
                 >
                     <FilterListIcon />
                 </IconButton>
+                <div className={`filter-box ${openFilter ? 'show-filters' : ''}`}>
+                    <h4>Filters</h4>
+                    <div className='card-type'>
+                        <p>Type</p>
+                        <div className='checkbox'>
+                            <FormControlLabel control={<Checkbox style={{color : "red"}}/>} label="Subscription" />
+                            <FormControlLabel control={<Checkbox style={{color : "red"}}/>} label="Burner" />
+                        </div>
+                    </div>
+                    <div className='card-holder'>
+                        <p>Cardholder</p>
+                        <Select placeholder="Select Cardholder" allowClear style={{ width: "100%" }}>
+                            <Option value="Aakash">Aakash</Option>
+                            <Option value="Kartik">Kartik</Option>
+                            <Option value="Hritik">Hritik</Option>
+                        </Select>
+                    </div>
+                    <div className='buttons'>
+                        <button>Apply</button>
+                        <button>Clear</button>
+                    </div>
+                </div>
             </div>
         </div>
         <div className='card-container'>
